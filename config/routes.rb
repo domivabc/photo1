fbfb26e730ae012513cd1a376eea0710ede4afd1
vagner domingues <domivabc@gmail.com>
2021-04-18 03:37:10 -0400

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :users
+  get 'home/index'
+  get "/user", to: "user#index"
+
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
